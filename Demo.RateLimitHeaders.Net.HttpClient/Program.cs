329cc498039980177efd4c68f2f0a5370c6891c5
Tomasz Pęczek <tpeczek@gmail.com>
2022-07-26 23:54:42 +0200

    Adding rate limited HttpClient sample

---
+++ Demo.RateLimitHeaders.Net.HttpClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Demo.RateLimitHeaders.Net.HttpClient;
+
+HttpClient client = new(new RateLimitPolicyHandler());
+client.BaseAddress = new("http://localhost:5262");
+
+while (true)
+{
+    Console.Write("{0:hh:mm:ss}: ", DateTime.UtcNow);
+
+    int nextRequestDelay = 1;
+
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync("/");
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+        else
+        {
+            Console.Write($"{(int)response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
+
+            string? retryAfter = response.Headers.GetValues("Retry-After").FirstOrDefault();
+            if (Int32.TryParse(retryAfter, out nextRequestDelay))
+            {
+                Console.Write($" | Retry-After: {nextRequestDelay}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    await Task.Delay(TimeSpan.FromSeconds(nextRequestDelay));
+}
---
+++ Demo.RateLimitHeaders.Net.HttpClient/RateLimitPolicyHandler.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Globalization;
+using System.Threading.RateLimiting;
+using System.Text.RegularExpressions;
+
+namespace Demo.RateLimitHeaders.Net.HttpClient
+{
+    internal class RateLimitPolicyHandler : DelegatingHandler
+    {
+        private string? _rateLimitPolicy;
+        private RateLimiter? _rateLimiter;
+
+        private static readonly Regex RATE_LIMIT_POLICY_REGEX = new Regex(@"(\d+);w=(\d+)", RegexOptions.Compiled);
+
+        public RateLimitPolicyHandler() : base(new HttpClientHandler())
+        { }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (_rateLimiter is not null)
+            {
+                using var rateLimitLease = await _rateLimiter.WaitAsync(1, cancellationToken);
+                if (rateLimitLease.IsAcquired)
+                {
+                    return await base.SendAsync(request, cancellationToken);
+                }
+
+                var rateLimitResponse = new HttpResponseMessage(HttpStatusCode.TooManyRequests);
+                rateLimitResponse.Content = new StringContent($"Service rate limit policy ({_rateLimitPolicy}) exceeded!");
+
+                if (rateLimitLease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                {
+                    rateLimitResponse.Headers.Add("Retry-After", ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo));
+                }
+
+                return rateLimitResponse;
+            }
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.Headers.Contains("RateLimit-Policy"))
+            {
+                _rateLimitPolicy = response.Headers.GetValues("RateLimit-Policy").FirstOrDefault();
+
+                if (_rateLimitPolicy is not null)
+                {
+                    Match rateLimitPolicyMatch = RATE_LIMIT_POLICY_REGEX.Match(_rateLimitPolicy);
+
+                    if (rateLimitPolicyMatch.Success)
+                    {
+                        int limit = Int32.Parse(rateLimitPolicyMatch.Groups[1].Value);
+                        int window = Int32.Parse(rateLimitPolicyMatch.Groups[2].Value);
+
+                        _rateLimiter = new FixedWindowRateLimiter(new FixedWindowRateLimiterOptions(
+                            limit,
+                            QueueProcessingOrder.NewestFirst,
+                            0,
+                            TimeSpan.FromSeconds(window),
+                            true
+                        ));
+
+                        string? rateLimitRemaining = response.Headers.GetValues("RateLimit-Remaining").FirstOrDefault();
+                        if (Int32.TryParse(rateLimitRemaining, out int remaining))
+                        {
+                            using var rateLimitLease = await _rateLimiter.WaitAsync(limit - remaining, cancellationToken);
+                        }
+                    }
+                }
+            }
+
+            return response;
+        }
+    }
+}
